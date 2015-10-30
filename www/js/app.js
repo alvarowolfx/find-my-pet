125d272e1122d9f197feccec7eb1bc410f0399b4
Alvaro Viebrantz <alvarowolfx@gmail.com>
2015-10-29 22:58:58 -0300

    Add listing posts

--- www/js/app.js
+++ www/js/app.js
@@ -6,14 +6,96 @@
 angular.module('starter', ['ionic'])
 
 .run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
+    $ionicPlatform.ready(function() {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if (window.StatusBar) {
+            StatusBar.styleDefault();
+        }
+    });
 })
+
+.controller('MainController', ['$scope', function($scope) {
+    $scope.posts = [];
+
+    for (var i = 10; i >= 1; i--) {
+        $scope.posts.push({
+            id: i,
+            username: i,
+            user_img: "http://lorempixel.com/60/60/people/" + i,
+            img: "http://lorempixel.com/300/150/animals/" + i,
+            reward: i
+        });
+    }
+
+    $scope.getArray = function(num) {
+        return new Array(num);
+    };
+
+}])
+
+.directive('ngTimes', function($compile) {
+    return {
+        restrict: 'A',
+        link: link,
+        transclude: true,
+        scope: {
+            ngTimes: '=ngTimes'
+        }
+    };
+
+    function link(scope, element, attrs, controllers, transclude) {
+        var originalContent;
+
+        transclude(scope, function(content) {
+            originalContent = content.html();
+        });
+
+        scope.$watch('ngTimes', generate);
+
+        function generate() {
+            var content = "<span ng-repeat='r in getArray() track by $index'>";
+            content += originalContent;
+            content += "</span>";
+            var newContent = $compile(content)(scope);
+            element.replaceWith(newContent);
+            element = newContent;
+        }
+
+        scope.getArray = function() {
+            return new Array(scope.ngTimes);
+        };
+    }
+})
+
+.directive('imgLoader', ['$compile', function($compile) {
+    return {
+        restrict: 'A',
+        link: link,
+        replace: true,
+        template: '<ion-spinner/>'
+    };
+
+    function link(scope, element, attrs, controllers) {
+        scope.$watch('imgLoader', function(oldValue,newValue) {
+            if(oldValue !== newValue){
+              loadImage();
+            }
+        });
+        loadImage();
+
+        function loadImage() {
+            img = new Image();
+            img.src = attrs.imgLoader;
+            img.onload = function() {
+                var imgEl = document.createElement('img');
+                imgEl.src = attrs.imgLoader;
+                element.replaceWith(imgEl);
+                element = imgEl;
+            };
+        }
+    }
+}]);
