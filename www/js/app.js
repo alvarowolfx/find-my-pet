0b5323d78a474233e82c69c907e82a254ce794d3
Alvaro Viebrantz <alvarowolfx@gmail.com>
2015-11-09 13:54:13 -0300

    Add firebase integration

--- www/js/app.js
+++ www/js/app.js
@@ -3,109 +3,113 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic', 'ui.utils.masks', 'ngCordova'])
+var app = angular.module('starter', ['ionic', 'ui.utils.masks', 'ngCordova', 'firebase']);
 
-  .run(function ($ionicPlatform) {
-    $ionicPlatform.ready(function () {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      if (window.cordova && window.cordova.plugins.Keyboard) {
-        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      }
-      if (window.StatusBar) {
-        StatusBar.styleDefault();
-      }
-    });
-  })
+app.run(['$ionicPlatform', InitApp]);
+app.controller('MainController', ['$scope', '$ionicModal', 'Post', '$ionicLoading', MainController]);
+app.constant("FirebaseURL", "https://scorching-fire-4975.firebaseio.com");
+app.factory('Post', ['User', 'FirebaseURL', '$firebaseArray', Post]);
+app.factory('User', ['$cordovaDevice', User]);
 
-  .controller('MainController', ['$scope', '$ionicModal', 'Post', function ($scope, $ionicModal, Post) {
-    $scope.posts = Post.all();
-    $scope.showMap = false;
-
-    $scope.toggleMap = function () {
-      $scope.showMap = !$scope.showMap;
-    };
-
-    $scope.createPost = function () {
-      $ionicModal.fromTemplateUrl('templates/modal-create-post.html', {
-        scope: $scope,
-        animation: 'slide-in-up'
-      }).then(function (modal) {
-        $scope.newPost = {
-          reward: 5
-        };
-
-        $scope.modal = modal;
-        $scope.modal.show();
-      });
-    };
-
-    $scope.savePost = function (form) {
-      var post = angular.copy($scope.newPost);
-      if (form.$valid && post.title) {
-        Post.create(post);
-        $scope.posts = Post.all();
-        $scope.closeModal();
-      }
-    };
-
-    $scope.closeModal = function () {
-      $scope.modal.remove();
-    };
-
-  }])
-
-  .factory('Post', ['User', function (User) {
-    var service = {
-      all: all,
-      create: create
-    };
-
-    var posts = [];
-    var qtdePosts = 5;
-    for (var i = qtdePosts; i >= 1; i--) {
-      posts.unshift(newTestPost(i, i));
+function InitApp($ionicPlatform) {
+  $ionicPlatform.ready(function () {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
-
-    return service;
-
-    function all() {
-      return posts;
+    if (window.StatusBar) {
+      StatusBar.styleDefault();
     }
+  });
+}
 
-    function newTestPost(title, reward) {
-      var id = posts.length;
-      return {
-        id: id,
-        title: title,
-        img: "http://lorempixel.com/300/150/animals/" + id,
-        reward: reward
+function MainController($scope, $ionicModal, Post, $ionicLoading) {
+  $scope.posts = Post.all();
+  $ionicLoading.show();
+  $scope.posts.$loaded().finally(function(){
+    $ionicLoading.hide();
+  });
+  //postsRef.$bindTo($scope,"posts");
+
+  $scope.showMap = false;
+
+  $scope.toggleMap = function () {
+    $scope.showMap = !$scope.showMap;
+  };
+
+  $scope.createPost = function () {
+    $ionicModal.fromTemplateUrl('templates/modal-create-post.html', {
+      scope: $scope,
+      animation: 'slide-in-up'
+    }).then(function (modal) {
+      $scope.newPost = {
+        reward: 5
       };
-    }
 
-    function create(post) {
-      var id = posts.length;
-      post.id = id;
-      post.img = "http://lorempixel.com/300/150/animals/" + id;
-      post.user_id = User.getUserIdentifier();
-      posts.unshift(post);
-    }
-  }])
-  .factory('User', ['$cordovaDevice', function($cordovaDevice){
-    var service = {
-      getUserIdentifier: getUserIdentifier
-    };
+      $scope.modal = modal;
+      $scope.modal.show();
+    });
+  };
 
-    var userId;
-    try {
-      userId = $cordovaDevice.getUUID();
-    }catch(e) {
-      userId = "FAKE_USER_ID_" + Math.random();
+  $scope.savePost = function (form) {
+    var post = angular.copy($scope.newPost);
+    if (form.$valid && post.title) {
+      Post.create(post);
+      $scope.posts = Post.all();
+      $scope.closeModal();
     }
+  };
 
-    return service;
+  $scope.closeModal = function () {
+    $scope.modal.remove();
+  };
 
-    function getUserIdentifier() {
-      return userId;
-    }
-  }]);
+};
+
+function Post(User, FirebaseURL, $firebaseArray) {
+  var service = {
+    all: all,
+    create: create
+  };
+
+  var postsRef = new Firebase(FirebaseURL + '/posts');
+
+  return service;
+
+  function all() {
+    return $firebaseArray(postsRef);
+  }
+
+  function create(post) {
+    var posts = all();
+    var id = posts.length + 1;
+    post.id = id;
+    post.img = "http://lorempixel.com/300/150/animals/" + id;
+    post.user_id = User.getUserIdentifier();
+    post.created_at = (new Date()).toISOString();
+
+    var item = postsRef.push();
+    item.setWithPriority(post, 0 - Date.now());
+    //posts.$add(post);
+  }
+}
+
+function User($cordovaDevice) {
+  var service = {
+    getUserIdentifier: getUserIdentifier
+  };
+
+  var userId;
+  try {
+    userId = $cordovaDevice.getUUID();
+  } catch (e) {
+    userId = "FAKE_USER_ID_" + Math.random();
+  }
+
+  return service;
+
+  function getUserIdentifier() {
+    return userId;
+  }
+}
